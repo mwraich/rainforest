d50a07af974f3a5d86002e388bf52af832a18f67
mwraich <mwraich28@gmail.com>
2016-02-01 17:57:48 -0500

    Added associations for Users, Reviews and Products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/show'
+
+  get 'reviews/create'
+
+  get 'reviews/destroy'
+
   resources :products
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
