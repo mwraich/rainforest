a90ab751a413e06ebadcfa52e541326a9107000a
mwraich <mwraich28@gmail.com>
2016-02-18 20:26:57 -0500

    replaced prevent default with get script

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,16 +1,20 @@
 $(document).on('ready page:load', function() {
-  $('#search-form').submit(function(e){
-    e.preventDefault();
-    var searchValue = $('#search').val();
+  $('#search-form').submit(
+    $.getScript('/products?search=' + searchValue);
 
     $.get('/products?search=' + searchValue)
       .done(function(data){
         console.log(data);
         $('#products').html(data);
       });
-
   });
 });
+//
+// $(document).on('ready page:load', function() {
+//   $('#search-form').submit(
+//     function(e){
+//     e.preventDefault();
+//     var searchValue = $('#search').val();
     // $.ajax({
     //   url: '/products?search=' + searchValue
     //   type: 'GET'
