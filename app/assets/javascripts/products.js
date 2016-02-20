f013c7d021df09c92425ec0d76f10aed9ddfd14e
mwraich <mwraich28@gmail.com>
2016-02-20 16:16:17 -0500

    Add kaminari gem, intalized beginning steps for infinite scroll, seed database

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,8 +1,16 @@
 $(document).on('ready page:load', function() {
-  $('#search-form').submit(function (){
+  $('#search-form').submit(function (e){
+    event.preventDefault();
     var searchValue = $('#search').val();
+
     $.getScript('/products?search=' + searchValue);
   });
+
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+      return alert('near bottom');
+    }
+  })
 });
 //
 // $(document).on('ready page:load', function() {
